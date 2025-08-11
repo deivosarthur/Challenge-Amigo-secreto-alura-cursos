283d34936bdff335c3b0d1235a9bb3a51ce4a087
Adolfo <adolfo.hormazabal@gmail.com>
2025-08-11 19:17:59 -0400

    Se comentaron las lineas de codigo de app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-// Declaramos un array 
+// Declaramos un array .
 let amigos = [];
 
 // Función que se ejecuta cuando el usuario hace clic en el botón "Añadir"
